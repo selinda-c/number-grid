817f3aceb04b2f457c0dfc8037828bb170f27b8b
Selinda-C <selindachapman@icloud.com>
2022-06-12 17:45:18 -0700

    add colors for odd/even

--- script/script.js
+++ script/script.js
@@ -1,12 +1,13 @@
 window.onload = function(){
   // clear backgroundColor of composite numbers
   document.querySelector('button#prime').onclick = function(){
+    document.getElementById('n1').style.backgroundColor = null;
     document.getElementById('n4').style.backgroundColor = null;
     document.getElementById('n6').style.backgroundColor = null;
     document.getElementById('n8').style.backgroundColor = null;
     document.getElementById('n9').style.backgroundColor = null;
     document.getElementById('n10').style.backgroundColor = null;
-    // turn on prime color backgrounds
+    // highlights prime nums
     document.getElementById('n2').style.backgroundColor = 'pink';
     document.getElementById('n3').style.backgroundColor = 'pink';
     document.getElementById('n5').style.backgroundColor = 'pink';
@@ -14,6 +15,7 @@ window.onload = function(){
   };
   // start of click for composite nums
   document.querySelector('button#composite').onclick = function(){
+    document.getElementById('n1').style.backgroundColor = null;
     document.getElementById('n2').style.backgroundColor = null;
     document.getElementById('n3').style.backgroundColor = null;
     document.getElementById('n5').style.backgroundColor = null;
@@ -25,4 +27,33 @@ window.onload = function(){
     document.getElementById('n9').style.backgroundColor = 'lightgreen';
     document.getElementById('n10').style.backgroundColor = 'lightgreen';
   };
+  //click for odd numbers
+  document.querySelector('button#odd').onclick = function(){
+    document.getElementById('n2').style.backgroundColor = null;
+    document.getElementById('n4').style.backgroundColor = null;
+    document.getElementById('n6').style.backgroundColor = null;
+    document.getElementById('n8').style.backgroundColor = null;
+    document.getElementById('n9').style.backgroundColor = null;
+    document.getElementById('n10').style.backgroundColor = null;
+    //highlights odd nums
+    document.getElementById('n1').style.backgroundColor = 'grey';
+    document.getElementById('n3').style.backgroundColor = 'grey';
+    document.getElementById('n5').style.backgroundColor = 'grey';
+    document.getElementById('n7').style.backgroundColor = 'grey';
+    document.getElementById('n9').style.backgroundColor = 'grey';
+  }
+  // click for even numbers
+  document.querySelector('button#even').onclick = function(){
+    document.getElementById('n1').style.backgroundColor = null;
+    document.getElementById('n3').style.backgroundColor = null;
+    document.getElementById('n5').style.backgroundColor = null;
+    document.getElementById('n7').style.backgroundColor = null;
+    document.getElementById('n9').style.backgroundColor = null;
+    // highlight even nums
+    document.getElementById('n2').style.backgroundColor = 'yellow';
+    document.getElementById('n4').style.backgroundColor = 'yellow';
+    document.getElementById('n6').style.backgroundColor = 'yellow';
+    document.getElementById('n8').style.backgroundColor = 'yellow';
+    document.getElementById('n10').style.backgroundColor = 'yellow';
+  }
 };
